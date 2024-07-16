949be104be084dd2352aabf0a96962613fd8c396
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-16 09:17:04 +0600

    Add api response model

---
+++ LibraryAPI/Models/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace LibraryAPI.Models
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+    }
+}
