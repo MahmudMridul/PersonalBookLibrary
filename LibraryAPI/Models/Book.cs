e82f4a5b2dde6092ccf7b124d389d52d9893cdfa
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-16 10:08:58 +0600

    Add book model

---
+++ LibraryAPI/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public DateTime Published { get; set; }
+    }
+}
--- LibraryAPI/Models/User.cs
+++ LibraryAPI/Models/User.cs
@@ -1,6 +1,14 @@
-﻿namespace LibraryAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public byte[] PasswordSalt { get; set; } = null!;
     }
 }
