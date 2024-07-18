cb2d8f3e8943774d1b9b7ac584d519ef52077cc0
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-18 11:31:09 +0600

    Add usersbooks model for user book relationship

---
+++ LibraryAPI/Models/UsersBooks.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
+{
+    /* One user can have many books. 
+     * One book can belong to many users (different copy)
+     */
+    public class UsersBooks
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+    }
+}
