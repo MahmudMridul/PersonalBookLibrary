78c428dd3053dcacb35c588c05fee46f78313b6a
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-18 11:35:31 +0600

    Add book author relationship model

---
+++ LibraryAPI/Models/BooksAuthors.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryAPI.Models
+{
+    /* One book can have many authors
+     * One author can write many books
+     */
+    public class BooksAuthors
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
