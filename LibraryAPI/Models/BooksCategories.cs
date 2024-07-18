ec2f1ae55ad3bd7c10304ffbbaac0ee2bce9f48e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-18 11:33:51 +0600

    Add book and category relationship model

---
+++ LibraryAPI/Models/BooksCategories.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryAPI.Models
+{
+    /* One book can have multiple categories
+     * One category can belong to multiple books
+     */
+    public class BooksCategories
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
