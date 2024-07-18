64365956277258688efddc165a6b3bc888cce8f1
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-18 12:13:00 +0600

    Add read list

---
+++ LibraryAPI/Models/ReadList.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAPI.Models
+{
+    public class ReadList
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+    }
+}
