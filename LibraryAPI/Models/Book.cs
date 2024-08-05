98d3a777be829d1487fa3a2f83e2844437d92166
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-05 10:17:00 +0600

    Update book model

--- LibraryAPI/Models/Book.cs
+++ LibraryAPI/Models/Book.cs
@@ -7,8 +7,6 @@ namespace LibraryAPI.Models
         [Key]
         public int Id { get; set; }
         public string Title { get; set; } = null!;
-        public string Category { get; set; } = null!;
-        public string Author { get; set; } = null!;
         public DateTime Published { get; set; }
     }
 }
