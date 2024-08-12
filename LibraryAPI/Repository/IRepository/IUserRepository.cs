157ff93bf6adddb8ea78f37c48658489b47a11f2
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-12 16:09:12 +0600

    Add repository pattern

---
+++ LibraryAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using LibraryAPI.Models;
+using LibraryAPI.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> RegisterUser(User user);
+    }
+}
---
+++ LibraryAPI/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using LibraryAPI.Db;
+using LibraryAPI.Models;
+using LibraryAPI.Models.DTOs;
+using LibraryAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly LibraryContext _db;
+
+        public UserRepository(LibraryContext db) 
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _db.Users.ToListAsync();
+        }
+
+        public async Task<User> RegisterUser(User user)
+        {
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+    }
+}
