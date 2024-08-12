179b28d52c83f93fdac375203928b128f8c139cb
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-12 09:52:15 +0600

    Add password utils

---
+++ LibraryAPI/Utils/PasswordUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace LibraryAPI.Utils
+{
+    internal class PasswordUtils
+    {
+        internal static byte[] GenerateSalt(int size = 16)
+        {
+            byte[] salt = new byte[size];
+            RandomNumberGenerator.Fill(salt);
+            return salt;
+        }
+    }
+}
