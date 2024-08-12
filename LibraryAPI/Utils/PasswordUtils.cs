abf917e44eca3617ccf15d75d6428ee21085ad09
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-12 11:50:42 +0600

    Add password salt and hash function

--- LibraryAPI/Utils/PasswordUtils.cs
+++ LibraryAPI/Utils/PasswordUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace LibraryAPI.Utils
 {
@@ -7,8 +8,44 @@ namespace LibraryAPI.Utils
         internal static byte[] GenerateSalt(int size = 16)
         {
             byte[] salt = new byte[size];
-            RandomNumberGenerator.Fill(salt);
+            try
+            {
+                RandomNumberGenerator.Fill(salt);
+                return salt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             return salt;
+        }
+
+        internal static string HashPassword(string password, byte[] salt)
+        {
+            try
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                    byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+
+                    Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
+                    Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
+
+                    byte[] hashedBytes = sha256.ComputeHash(saltedPassword);
+
+                    byte[] hashedPassWithSalt = new byte[hashedBytes.Length + salt.Length];
+                    Buffer.BlockCopy(salt, 0, hashedPassWithSalt, 0, salt.Length);
+                    Buffer.BlockCopy(hashedBytes, 0, hashedPassWithSalt, salt.Length, hashedBytes.Length);
+
+                    return Convert.ToBase64String(hashedPassWithSalt);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return "";
         }
     }
 }
