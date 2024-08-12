e5a971ade6590623d325764fec039d353f645b0e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-12 09:52:42 +0600

    Add user registration DTO

---
+++ LibraryAPI/Models/DTOs/UserRegistrationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAPI.Models.DTOs
+{
+    public class UserRegistrationDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
