2d660a96bffac8620b8bd4a8ba415e08b412c7b7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-13 10:03:57 +0600

    Add TokenUtils class and GetToken function

---
+++ LibraryAPI/Utils/TokenUtils.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LibraryAPI.Utils
+{
+    internal class TokenUtils
+    {
+        internal static string GetToken(string name, string secretKey)
+        {
+            JwtSecurityTokenHandler hanlder = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(secretKey);
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, name)
+                    }
+                ),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            SecurityToken token = hanlder.CreateToken(descriptor);
+            return hanlder.WriteToken(token);
+        }
+    }
+}
